2b338e987da9225e326c4a12e75b0806a05647f1
Brendan <Granipouss@gmail.com>
2020-08-12 04:23:42 +0200

    allow unused vars

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,4 +3,19 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+
+  rules: {
+    'jsx-a11y/href-no-hash': 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-expressions': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        vars: 'all',
+        args: 'after-used',
+        ignoreRestSiblings: true,
+        argsIgnorePattern: '^_', // ignore unused variables whose name is '_'
+      },
+    ],
+  },
 };
